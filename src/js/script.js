a9ae9d61a3f11eeaae796b2071c492f358f533a7
Aleksandr <generalovshura9@gmail.com>
2023-09-20 21:01:16 +0300

    first

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  closeElem = document.querySelector(".menu_close");
+
+function openHamb() {
+  hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
+  });
+}
+
+function closeHamb() {
+  closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+}
+
+openHamb();
+closeHamb();
