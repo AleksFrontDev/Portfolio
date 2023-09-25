7362966e193ae0c0ed8325feef7205016b371aff
Aleksandr <generalovshura9@gmail.com>
2023-09-25 11:10:18 +0300

    itsWorking

---
+++ dist/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";window.addEventListener("DOMContentLoaded",(function(){!function(){const e=document.querySelector(".hamburger"),t=document.querySelector(".menu"),c=document.querySelector(".menu_close");e.addEventListener("click",(()=>{t.classList.add("active")})),c.addEventListener("click",(()=>{t.classList.remove("active")}))}()}))})();
+//# sourceMappingURL=bundle.js.map
