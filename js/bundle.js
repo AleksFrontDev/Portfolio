39c02fe2898e8eea4bb32058a40ca0cf6e9e3343
Aleksandr <generalovshura9@gmail.com>
2023-12-06 09:26:37 +0200

    added cariuesel

--- js/bundle.js
+++ js/bundle.js
@@ -1,2 +1,119 @@
-(()=>{"use strict";var e={530:(e,o,t)=>{t.d(o,{Z:()=>r});const r=function(){const e=document.querySelector(".hamburger"),o=document.querySelector(".menu"),t=document.querySelector(".menu_close");e.addEventListener("click",(()=>{o.classList.add("active"),console.log("Menu opened")})),t.addEventListener("click",(()=>{o.classList.remove("active"),console.log("Menu closed")}))}},598:(e,o,t)=>{var r=t(530);window.addEventListener("DOMContentLoaded",(function(){(0,r.Z)(),console.log("Hamburger module loaded")}))}},o={};function t(r){var n=o[r];if(void 0!==n)return n.exports;var c=o[r]={exports:{}};return e[r](c,c.exports,t),c.exports}t.d=(e,o)=>{for(var r in o)t.o(o,r)&&!t.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:o[r]})},t.o=(e,o)=>Object.prototype.hasOwnProperty.call(e,o),t(598),t(530)})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/modules/hamburger.js":
+/*!*************************************!*\
+  !*** ./src/js/modules/hamburger.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+function hamburger() {
+  const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu_close");
+  function openHamb() {
+    hamburger.addEventListener("click", () => {
+      menu.classList.add("active");
+      console.log("Menu opened");
+    });
+  }
+  function closeHamb() {
+    closeElem.addEventListener("click", () => {
+      menu.classList.remove("active");
+      console.log("Menu closed");
+    });
+  }
+  openHamb();
+  closeHamb();
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (hamburger);
+
+/***/ }),
+
+/***/ "./src/js/script.js":
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_hamburger__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/hamburger */ "./src/js/modules/hamburger.js");
+
+window.addEventListener("DOMContentLoaded", function () {
+  (0,_modules_hamburger__WEBPACK_IMPORTED_MODULE_0__["default"])();
+  console.log("Hamburger module loaded");
+});
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_require__("./src/js/script.js");
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/modules/hamburger.js");
+/******/ 	
+/******/ })()
+;
 //# sourceMappingURL=bundle.js.map
